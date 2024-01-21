04f76642c05f5c0e2848cbc1755618a5dbf5d289
Cfp <chaosflo44plus@gmail.com>
2024-01-21 11:25:22 +0100

    🐛 Time out devices after 10 seconds

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@
 import type { ServerWebSocket, Socket } from "bun";
 import { z } from "zod";
 
-const sockets: {[serial: string]: { socket: Socket }} = {};
+const sockets: {[serial: string]: { socket: Socket, lastPacket: Date }} = {};
 const websockets: { socket: ServerWebSocket<unknown>, serials: string[] }[] = [];
 
 Bun.listen({
@@ -22,6 +22,11 @@ Bun.listen({
 			try {
 				console.log(data.toString())
 				const packet = JSON.parse(data.toString());
+
+				const mySocket = Object.keys(sockets).find(serial => sockets[serial].socket === socket);
+				if(mySocket) {
+					sockets[mySocket].lastPacket = new Date();
+				}
 				
 				// if("serial" in packet) {
 				// 	if(!(packet.serial in sockets)) {
@@ -32,17 +37,17 @@ Bun.listen({
 					const sensorpacket = SensorPacket.parse(packet);
 
 					if(!(sensorpacket.serial in sockets)) {
-						sockets[sensorpacket.serial] = { socket };
+						console.log("[OPENING] Opening " + sensorpacket.serial);
+						sockets[sensorpacket.serial] = { socket, lastPacket: new Date() };
 					}
 				}
 				
 				// Broadcast to all clients with the same serial
-				const mySerial = Object.keys(sockets).find(serial => sockets[serial].socket === socket);
-				if(!mySerial) {
-					socket.write(JSON.stringify({ error: 1, message: "Serial not found" }));
+				if(!mySocket) {
+					socket.write(JSON.stringify({ error: 1, message: "You dont have a serial yet" }));
 					return;
 				}
-				for(const websocket of websockets.filter(websocket => websocket.serials.includes(mySerial))) {
+				for(const websocket of websockets.filter(websocket => websocket.serials.includes(mySocket))) {
 					websocket.socket.send(data.toString());
 				}
 			} catch (error) {
@@ -54,23 +59,48 @@ Bun.listen({
 			// Remove socket from sockets
 			for(const serial in sockets) {
 				if(sockets[serial].socket === socket) {
+					console.log("[CLOSING] Closing " + serial);
 					delete sockets[serial];
 				}
 			}
 		}, // Client disconnected
 		drain(socket) {}, // Socket ready for more data, which means the data sent to client is sent successfully
-		error(socket, error) {} // Socket error
+		error(socket, error) {
+			// Remove socket from sockets
+			for(const serial in sockets) {
+				if(sockets[serial].socket === socket) {
+					console.log("[CLOSING] Socket error for " + serial);
+					delete sockets[serial];
+				}
+			}
+		} // Socket error
 	}
 })
+
+setInterval(() => {
+	const now = new Date();
+	for(const serial in sockets) {
+		// Remove socket if last packet was more than 10 seconds ago
+		if(now.getTime() - sockets[serial].lastPacket.getTime() > 10000) {
+			// Disconnect all websockets with this serial
+			console.log("[CLOSING] Serial " + serial + " timed out");
+			for(const websocket of websockets.filter(websocket => websocket.serials.includes(serial))) {
+				websocket.socket.close();
+			}
+			sockets[serial].socket.end();
+			delete sockets[serial];
+		}
+	}
+});
 
 Bun.serve({
 	port: 8080,
   fetch(req, server) {
     // upgrade the request to a WebSocket
-    if (server.upgrade(req)) {
+    if(server.upgrade(req)) {
       return; // do not return a Response
     }
-    return new Response("Upgrade failed :(", { status: 500 });
+    return new Response("", { status: 500 });
   },
   websocket: {
 		open(ws) {
